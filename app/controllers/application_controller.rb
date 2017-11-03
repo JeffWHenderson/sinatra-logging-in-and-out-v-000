d4371a934973e9ff925e1a8334abd8e570dd372b
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-03 06:21:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
       redirect :'/account'
     else
       erb :'error'
-    end             #shows the error page if username and ID do not match available users
+    end       
   end
 
   get '/account' do
@@ -35,6 +35,4 @@ class ApplicationController < Sinatra::Base
     session.clear
     redirect :'/'
   end
-
-
 end
