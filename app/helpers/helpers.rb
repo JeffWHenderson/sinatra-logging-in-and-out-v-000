00cee010dcbc423c2b7e492b6e49caa951b706c5
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-03 06:02:20 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
 
   def current_user(session)
-     User.find_by(session[:user])
+     User.find_by(session[:user_id])
   end
 
   def is_logged_in?(session)
-    
+    !!session[user_id]
   end
 end
