a7d1241aa95ef93499465e35baaf3c897e77eded
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-03 06:03:08 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 class Helpers
 
   def current_user(session)
-     User.find_by(session[:user_id])
+     User.find(session_hash[:user_id])#User.find_by(session[:user_id])
   end
 
   def is_logged_in?(session)
