b01d918095fe0a47b4d6ca31c2580a315608ef8f
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-03 04:50:12 +0000

    automatically backed up by learn

--- db/migrate/20171103044455_create_users.rb
+++ db/migrate/20171103044455_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.sting :real
+      t.real :balance
     end
   end
 end
